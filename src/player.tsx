7842b67338aa8c165fc65671901c64c888ffb178
semiexp <linthya.v@gmail.com>
2025-02-22 23:50:07 +0900

    restart, history

--- src/player.tsx
+++ src/player.tsx
@@ -8,36 +8,47 @@ export type PlayerProps<T, P extends Puzzle<T>> = {
 
 export const Player = <T, P extends Puzzle<T>>(props: PlayerProps<T, P>) => {
   const [puzzle, setPuzzle] = useState(props.puzzle.asPlayMode());
+  const [history, setHistory] = useState<P[]>([]);
 
   // add key event listener
   const onKeyDown = (event: KeyboardEvent) => {
-    setPuzzle((puzzle) => {
-      let nextPuzzle = null;
-      switch (event.key) {
-        case "ArrowUp":
-        case "w":
-          nextPuzzle = puzzle.move("up");
-          break;
-        case "ArrowDown":
-        case "s":
-          nextPuzzle = puzzle.move("down");
-          break;
-        case "ArrowLeft":
-        case "a":
-          nextPuzzle = puzzle.move("left");
-          break;
-        case "ArrowRight":
-        case "d":
-          nextPuzzle = puzzle.move("right");
-          break;
-      }
+    if (event.key === "r") {
+      setPuzzle(props.puzzle.asPlayMode());
+      setHistory([]);
+      return;
+    }
 
-      if (nextPuzzle === null) {
-        return puzzle;
-      } else {
-        return nextPuzzle;
+    if (event.key === "z") {
+      if (history.length > 0) {
+        setPuzzle(history[history.length - 1]);
+        setHistory(history.slice(0, -1));
       }
-    });
+      return;
+    }
+
+    let nextPuzzle = null;
+    switch (event.key) {
+      case "ArrowUp":
+      case "w":
+        nextPuzzle = puzzle.move("up");
+        break;
+      case "ArrowDown":
+      case "s":
+        nextPuzzle = puzzle.move("down");
+        break;
+      case "ArrowLeft":
+      case "a":
+        nextPuzzle = puzzle.move("left");
+        break;
+      case "ArrowRight":
+      case "d":
+        nextPuzzle = puzzle.move("right");
+        break;
+    }
+    if (nextPuzzle !== null) {
+      setPuzzle(nextPuzzle);
+      setHistory([...history, puzzle]);
+    }
   };
 
   useEffect(() => {
@@ -45,7 +56,7 @@ export const Player = <T, P extends Puzzle<T>>(props: PlayerProps<T, P>) => {
     return () => {
       window.removeEventListener("keydown", onKeyDown);
     };
-  }, []);
+  }, [puzzle]);
 
   const unitSize = 40;
   const [renderHeight, renderWidth] = puzzle.renderSize(unitSize);
